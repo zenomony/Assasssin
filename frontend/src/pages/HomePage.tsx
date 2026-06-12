4681fcfa54eb854f839d962c7dc706091f8a55ba
Hunter <hunter.tipton9141@gmail.com>
2026-06-11 22:29:33 -0400

    Adding navigation to Login and Register Pages

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Assassin</h1>
 
-      <button>Login</button>
+      <div>
+        <Link to="/login">
+            <button>Login</button>
+        </Link>
+      </div>
 
-      <button>Register</button>
+      <div>
+        <Link to="/register">
+            <button>Register</button>
+        </Link>
+      </div>
     </div>
   );
 }
